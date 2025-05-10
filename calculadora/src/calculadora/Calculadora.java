7f3e7f62bb8e21818d32d5e4dea91326b52db9f8
jmcardoso18 <jmc18.ads@gmail.com>
2025-05-10 10:17:01 -0300

    Incluindo o arquivo inicial

---
+++ calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author tibd8
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tela t = new tela();
+        t.setVisible(true);
+    }
+    
+}
